08998a0df46470fc04031822a181a87b115cc61d
kovec681 <49417329+kovec681@users.noreply.github.com>
2019-10-14 21:30:33 -0700

    Add files via upload
    main.js is a dummy script to load a new instance of a GameController class defined in gamecontroller.js
    
    GameController requires PixiJS version 5.1.3

---
+++ gamecontroller.js
@@ -0,0 +1,64 @@
+// gamecontroller.js
+
+/*
+ * 
+ */
+
+// Alias Declarations
+let AnimatedSprite = PIXI.AnimatedSprite;
+let Container = PIXI.Container;
+let Loader = PIXI.Loader;
+let Sprite = PIXI.Sprite;
+let sound = PIXI.sound;
+let Texture = PIXI.Texture;
+let Text = PIXI.Text;
+let TextStyle = PIXI.TextStyle;
+
+
+class GameController {
+    
+    /*
+     * Initialize a game that takes place on STAGE with dimensions WIDTH X
+     *  HEIGHT
+     * 
+     * STAGE: PIXI Container where the game will take place
+     * WIDTH: Width of the app in pixels
+     * HEIGHT: Height of the app in pixels
+     */
+    constructor(STAGE, WIDTH, HEIGHT) {
+        this.stage = STAGE;
+        this.width = WIDTH;
+        this.height = HEIGHT;
+        
+        this.gameActive = true;
+        
+        this.container = new Container();
+        
+        this.assetLoader = new Loader();
+    }
+    
+    setup() {
+        this.loadAssets();
+        this.setupBackdrop();
+    }
+    
+    loadAssets() {
+        this.assetLoader.add("backdrop.png");
+        this.assetLoader.add("ui_bg.png");
+        this.assetLoader.add("ghost.png");
+    }
+    
+    setupBackdrop() {
+        let backdrop = new Sprite(Texture.from("backdrop.png"));
+        this.container.addChild(backdrop);
+        this.stage.addChild(this.container);
+    }
+    
+    newGame() {
+        this.setup();
+    }
+    
+    isActive() {
+        return this.gameActive;
+    }
+}
