128c1d19ee4e361c16e9209b3bfbcf908b332a3b
saturdaaaaaaaay <ecc226@nau.edu>
2019-10-14 19:45:08 -0700

    Set up some more scene graphs
    The assets files are temporary btw

--- game.js
+++ game.js
@@ -5,23 +5,32 @@ gameport.appendChild(renderer.view);
 var stage = new PIXI.Container();
 
 //scene graphs
-var menuScene;
+var titleScene;
 var gameScene;
 var creditScene;
 
+//title text (this stuff's temporary)
+var titleText;
+var startText;
+
 //load spritesheet
 PIXI.loader
-  //.add("assets.json")
+  .add("assets.json")
   .load(setup);
 
 //set up game
 function setup()
 {
-  menuScene = new PIXI.Container();
+  titleScene = new PIXI.Container();
   gameScene = new PIXI.Container();
 
-  stage.addChild(menuScene);
+  stage.addChild(titleScene);
   stage.addChild(gameScene);
+
+  gameScene.visible = false;
+
+  titleSetup();
+  gameSetup();
 }
 
 function animate()
@@ -35,4 +44,36 @@ animate();
 function reset()
 {
 
+}
+
+function titleSetup()
+{
+  titleText = new PIXI.Text("Untitled Trick or Treat Game", {fill : 0xff1010});
+  startText = new PIXI.Text("Start", {fill : 0xff1010});
+
+  titleScene.addChild(titleText);
+  titleScene.addChild(startText);
+
+  titleText.position.x = 200;
+  titleText.position.y = 200;
+  startText.position.x = 200;
+  startText.position.y = 250;
+
+  startText.interactive = true;
+  startText.buttonMode = true;
+
+  startText.on('mousedown', dispGame);
+}
+
+function gameSetup()
+{
+  var tempText =  new PIXI.Text("Game: coming soon :)", {fill : 0xff1010});
+
+  gameScene.addChild(tempText);
+}
+
+function dispGame()
+{
+  gameScene.visible = true;
+  titleScene.visible = false;
 }
